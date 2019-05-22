78ab6883e9fd277b0339e30820b290e83f7e12e0
yang bo <yang.wilby@gmail.com>
2019-05-22 21:40:17 +0200

    Initial commit

---
+++ eniro.go
@@ -0,0 +1,124 @@
+// Package google provides a function to do Eniro searches using the Eniro Web
+// Search API. See https://developers.google.com/web-search/docs/
+//
+// This package is an example to accompany https://blog.golang.org/context.
+// It is not intended for use by others.
+//
+// Eniro has since disabled its search API,
+// and so this package is no longer useful.
+package main
+
+import (
+    "context"
+    "encoding/json"
+    "net/http"
+)
+const baseURL = "https://api.eniro.com/partnerapi/cs/search/basic"
+
+type Result struct {
+    Title string `json:"title"`
+    Query string `json:"query"`
+    TotalHits uint32 `json:"totalHits"`
+    TotalCount uint32 `json:"totalCount"`
+    StartIndex uint32 `json:"startIndex"`
+    ItemsPerPage uint16 `json:"itemsPerPage"`
+    Adverts []Advert `json:"adverts"`
+}
+
+type Advert struct {
+    EniroId string `json:"eniroId"`
+    CompanyInfo CompanyInfo `json:"companyInfo"`
+    Address Address `json:"address"`
+    Location Location `json:"location"`
+    PhoneNumbers PhoneNumbers `json:"phoneNumbers"`
+    CompanyReviews string `json:"companyReviews"`
+    Homepage string `json:"homepage"`
+    Facebook string `json:"facebook"`
+    InfoPageLink string `json:"infoPageLink"`
+}
+
+type CompanyInfo struct {
+    CompanyName string `json:"companyName"`
+    OrgNumber string `json:"orgNumber"`
+    CompanyText string `json:"companyText"`
+}
+
+type Address struct {
+    StreetName string `json:"streetName"`
+    PostCode string `json:"postCode"`
+    PostArea string `json:"postArea"`
+    PostBox string `json:"postBox"`
+}
+
+type Coordinate struct {
+    Use string `json:"use"`
+    Longitude float32 `json:"longitude"`
+    Latitude float64 `json:"latitude"`
+}
+type Coordinates []Coordinate
+
+type Location struct {
+    Coordinates Coordinates `json:"coordinates"`
+
+}
+type PhoneNumber struct {
+    Kind string `json:"type"`
+    PhoneNumber string `json:"phoneNumber"`
+    Label string `json:"label"`
+}
+type PhoneNumbers []PhoneNumber
+
+// Search sends query to Eniro search and returns the result.
+func Search(ctx context.Context, query string) (Result, error) {
+    // Prepare the Eniro Search API request.
+    //?profile=interview&key=114773894699415832&country=se&version=1.1.3&search_word=pizza
+
+    req, err := http.NewRequest("GET", baseURL, nil)
+    if err != nil {
+        return Result{}, err
+    }
+    q := req.URL.Query()
+    q.Set("profile", "interview")
+    q.Set("key", "114773894699415832")
+    q.Set("country", "se")
+    q.Set("version", "1.1.3")
+    q.Set("search_word", query)
+
+    req.URL.RawQuery = q.Encode()
+
+    // Issue the HTTP request and handle the response. The httpDo function
+    // cancels the request if ctx.Done is closed.
+    var result Result
+    err = httpDo(ctx, req, func(resp *http.Response, err error) error {
+        if err != nil {
+            return err
+        }
+        defer resp.Body.Close()
+        // unserialize the response to go struct
+        if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+            return err
+        }
+
+        return nil
+    })
+    // httpDo waits for the closure we provided to return, so it's safe to
+    // read result here.
+    return result, err
+}
+
+// httpDo issues the HTTP request and calls f with the response. If ctx.Done is
+// closed while the request or f is running, httpDo cancels the request, waits
+// for f to exit, and returns ctx.Err. Otherwise, httpDo returns f's error.
+func httpDo(ctx context.Context, req *http.Request, f func(*http.Response, error) error) error {
+    // Run the HTTP request in a goroutine and pass the response to f.
+    c := make(chan error, 1)
+    req = req.WithContext(ctx)
+    go func() { c <- f(http.DefaultClient.Do(req)) }()
+    select {
+    case <-ctx.Done():
+        <-c // Wait for f to return.
+        return ctx.Err()
+    case err := <-c:
+        return err
+    }
+}
---
+++ main.go
@@ -0,0 +1,79 @@
+// The server program issues Eniro search requests
+//
+// The /search endpoint accepts these query params:
+//   q=the Eniro search query
+//   timeout=a timeout for the request, in time.Duration format
+//
+// For example, http://localhost:8080/search?q=pizza&timeout=1s serves the
+// first few Eniro search results for "pizza" or a "deadline exceeded" error
+// if the timeout expires.
+package main
+
+import (
+    "context"
+    "encoding/json"
+    "flag"
+    "log"
+    "net/http"
+    "time"
+)
+var (
+    listenAddr = flag.String("listenAddr", ":8080", "Server address")
+)
+func main() {
+    flag.Parse()
+    http.HandleFunc("/search", handleSearch)
+    log.Fatal(http.ListenAndServe(*listenAddr, nil))
+}
+
+// handleSearch handles URLs like /search?q=pizza&timeout=1s by forwarding the
+// query to google.Search. If the query param includes timeout, the search is
+// canceled after that duration elapses.
+func handleSearch(w http.ResponseWriter, req *http.Request) {
+    // ctx is the Context for this handler. Calling cancel closes the
+    // ctx.Done channel, which is the cancellation signal for requests
+    // started by this handler.
+    var (
+        ctx    context.Context
+        cancel context.CancelFunc
+    )
+    timeout, err := time.ParseDuration(req.FormValue("timeout"))
+    if err == nil {
+        // The request has a timeout, so create a context that is
+        // canceled automatically when the timeout expires.
+        ctx, cancel = context.WithTimeout(context.Background(), timeout)
+    } else {
+        ctx, cancel = context.WithCancel(context.Background())
+    }
+    defer cancel() // Cancel ctx as soon as handleSearch returns.
+
+    // Check the search query.
+    query := req.FormValue("q")
+    if query == "" {
+        http.Error(w, "no query", http.StatusBadRequest)
+        return
+    }
+
+    // Run the Eniro search and print the results.
+    start := time.Now()
+    result, err := Search(ctx, query)
+    elapsed := time.Since(start)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    
+    var ret = struct {
+        Result          Result
+        Timeout, Elapsed time.Duration
+    }{
+        Result: result,
+        Timeout: timeout,
+        Elapsed: elapsed,
+    }
+    if err := json.NewEncoder(w).Encode(ret); err != nil {
+        log.Print(err)
+        return
+    }
+}
